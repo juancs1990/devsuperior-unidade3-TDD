88a37e23e971d5a8e8ab947cf90f909be7714aa3
juancs1990 <juancarlos.s2013@gmail.com>
2021-10-18 22:30:53 -0300

    first commit

---
+++ src/main/java/com/devsuperior/bds04/services/EventService.java
@@ -0,0 +1,67 @@
+package com.devsuperior.bds04.services;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.entities.Event;
+import com.devsuperior.bds04.repositories.CityRepository;
+import com.devsuperior.bds04.repositories.EventRepository;
+import com.devsuperior.bds04.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class EventService {
+	
+	@Autowired
+	private EventRepository repository;
+	
+	@Autowired
+	private CityRepository cityRepository;
+	
+	
+	@Transactional
+	public EventDTO update(Long id, EventDTO dto) {
+		
+		try {
+			Event entity = repository.getOne(id);
+			 entity.setName(dto.getName());
+			 entity.setDate(dto.getDate());
+			 entity.setCity(cityRepository.getOne(dto.getCityId()));
+			 entity.setUrl(dto.getUrl());
+			 entity = repository.save(entity);
+			return new EventDTO(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}		
+	}
+	
+	@Transactional
+	public EventDTO insert(EventDTO dto) {
+		  Event entity = new Event();
+		  entity.setName(dto.getName());
+		  entity.setUrl(dto.getUrl());
+		  entity.setDate(dto.getDate());
+		  entity.setCity(cityRepository.getOne(dto.getCityId()));
+		  repository.save(entity);
+		  return new EventDTO(entity);
+	}
+	
+	
+	@Transactional(readOnly = true)
+	public Page<EventDTO> findAllPaged(Pageable pageable) {
+		Page<Event> list = repository.findAll(pageable);
+		return list.map(x -> new EventDTO(x));
+	}
+
+		
+		
+		
+	}
+
+
